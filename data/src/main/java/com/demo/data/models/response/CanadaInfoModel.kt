9931cef7932e9ec6c187d5f9ccf86260399e6f2e
ashish <ashish.budhathoki@outlook.com>
2021-01-26 21:37:02 +1100

    Add models for data layer

---
+++ data/src/main/java/com/demo/data/models/response/CanadaInfoModel.kt
@@ -0,0 +1,10 @@
+package com.demo.data.models.response
+
+/**
+ * Representation for a [CanadaInfoModel] fetched from the API
+ */
+data class CanadaInfoModel(
+    val title: String,
+    val description: String,
+    val imageHref: String
+)
