cdb8fc7ae64b453235e847a9701187db2f8383e1
ashish <ashish.budhathoki@outlook.com>
2021-01-26 23:28:32 +1100

    Add BaseActivity and BaseFragment

---
+++ app/src/main/java/com/demo/aboutcanada/base/BaseFragment.kt
@@ -0,0 +1,32 @@
+package com.demo.aboutcanada.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import dagger.android.support.AndroidSupportInjection
+
+abstract class BaseFragment : Fragment() {
+
+    abstract fun layoutId(): Int
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        AndroidSupportInjection.inject(this)
+        return inflater.inflate(layoutId(), container, false)
+    }
+
+
+    open fun getActionBar(): ActionBar? {
+        return (activity as AppCompatActivity?)?.supportActionBar
+    }
+
+    open fun onBackPressed() {}
+
+}
